aa8b37f845662bf6089fe5a6a37f0c1ce326ad42
Gabriel Konat <gabrielkonat@gmail.com>
2021-03-31 15:46:08 +0200

    WIP graphics

--- core/util/src/timing.rs
+++ core/util/src/timing.rs
@@ -110,3 +110,86 @@ impl Timer {
     Time { elapsed, delta }
   }
 }
+
+
+pub struct FrameTimer {
+  timer: Timer,
+  frame: u64,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct FrameTime {
+  pub elapsed: Duration,
+  pub frame_time: Duration,
+  pub frame: u64,
+}
+
+impl FrameTimer {
+  pub fn new() -> FrameTimer { FrameTimer { timer: Timer::new(), frame: 0 } }
+
+  pub fn frame(&mut self) -> FrameTime {
+    let Time { elapsed, delta: frame_time } = self.timer.update();
+    let frame_time = FrameTime { elapsed, frame_time, frame: self.frame };
+    self.frame += 1;
+    frame_time
+  }
+}
+
+
+pub struct TickTimer {
+  tick: u64,
+  start: Instant,
+  time_target: Duration,
+  accumulated_lag: Duration,
+}
+
+impl TickTimer {
+  pub fn new(tick_time_target: Duration) -> TickTimer {
+    TickTimer {
+      tick: 0,
+      start: Instant::now(),
+      time_target: tick_time_target,
+      accumulated_lag: Duration::zero(),
+    }
+  }
+
+
+  pub fn update_lag(&mut self, frame_time: Duration) -> Duration {
+    self.accumulated_lag += frame_time;
+    self.accumulated_lag
+  }
+
+  pub fn num_upcoming_ticks(&self) -> u64 {
+    (self.accumulated_lag / self.time_target).floor() as u64
+  }
+
+  pub fn should_tick(&self) -> bool {
+    self.accumulated_lag >= self.time_target
+  }
+
+  pub fn tick_start(&mut self) -> u64 {
+    self.start = Instant::now();
+    self.tick
+  }
+
+  pub fn tick_end(&mut self) -> Duration {
+    self.tick += 1;
+    self.accumulated_lag -= self.time_target;
+    self.start.to(Instant::now())
+  }
+
+
+  pub fn time_target(&self) -> Duration {
+    self.time_target
+  }
+
+  pub fn accumulated_lag(&self) -> Duration {
+    self.accumulated_lag
+  }
+
+  pub fn extrapolation(&self) -> f64 {
+    let lag_ns = self.accumulated_lag.as_ns();
+    let target_ns = self.time_target.as_ns();
+    lag_ns as f64 / target_ns as f64
+  }
+}
--- graphics/triangle/src/main.rs
+++ graphics/triangle/src/main.rs
@@ -1,23 +1,89 @@
+use std::sync::mpsc::Receiver;
+use std::thread;
+
 use anyhow::{Context, Result};
+use dotenv;
+use tracing::debug;
+use tracing_subscriber::{EnvFilter, fmt};
+use tracing_subscriber::prelude::*;
 
 use math::prelude::*;
 use os::context::OsContext;
-use os::event_sys::OsEventSys;
+use os::event_sys::{OsEvent, OsEventSys};
 use os::input_sys::OsInputSys;
 use os::window::Window;
+use util::timing::{Duration, FrameTime, FrameTimer, TickTimer};
 
 fn main() -> Result<()> {
-  let mut os_context = OsContext::new();
+  // Load environment variables from .env file.
+  dotenv::dotenv().ok();
+  // Setup tracing
+  let fmt_layer = fmt::layer()
+    .with_writer(std::io::stderr)
+    ;
+  let filter_layer = EnvFilter::from_default_env();
+  tracing_subscriber::registry()
+    .with(filter_layer)
+    .with(fmt_layer)
+    .init();
+  // Setup OS context, window, and event system.
+  let os_context = OsContext::new();
   let window = {
     let window_min_size = LogicalSize::new(1920.0, 1080.0);
     Window::new(&os_context, window_min_size, window_min_size, "SG")
       .with_context(|| "Failed to create window")?
   };
-  let (mut os_event_sys, os_event_rx, os_input_sys) = {
+  let (os_event_sys, os_event_rx, os_input_sys) = {
     let (event_sys, input_event_rx, event_rx) = OsEventSys::new(&window);
     let input_sys = OsInputSys::new(input_event_rx);
     (event_sys, event_rx, input_sys)
   };
+  // Spawn application thread.
+  let _app_thread = thread::Builder::new()
+    .name("Triangle".to_string())
+    .spawn(move || {
+      debug!("Application thread started");
+      run(window, os_event_rx, os_input_sys)
+        .with_context(|| "Application thread stopped with an error").unwrap();
+      debug!("Application thread stopped");
+    })
+    .with_context(|| "Failed to create game thread")?;
+  // Run OS event system, hijacking the current thread. All code after the next line is ignored!
+  debug!("Main thread OS-event loop started");
   os_event_sys.run(os_context);
+  Ok(()) // Ignored, but needed to conform to the return type.
+}
+
+fn run(_window: Window, os_event_rx: Receiver<OsEvent>, mut os_input_sys: OsInputSys) -> Result<()> {
+  let mut frame_timer = FrameTimer::new();
+  let mut tick_timer = TickTimer::new(Duration::from_ns(16_666_667));
+  'main: loop {
+    // Timing
+    let FrameTime { frame_time, .. } = frame_timer.frame();
+    tick_timer.update_lag(frame_time);
+    // Process OS events
+    for os_event in os_event_rx.try_iter() {
+      match os_event {
+        OsEvent::TerminateRequested => break 'main,
+        OsEvent::WindowResized(_screen_size) => {
+          // TODO: resize
+          //gfx.screen_size_changed(screen_size);
+        }
+      }
+    }
+    // Process input
+    let _raw_input = os_input_sys.update();
+    // TODO: Simulate tick
+    if tick_timer.should_tick() {
+      while tick_timer.should_tick() { // Run simulation.
+        tick_timer.tick_start();
+        //sim.simulate(tick_timer.time_target());
+        tick_timer.tick_end();
+      }
+    }
+    // TODO: Render frame
+    //gfx.render_frame(&mut sim.world, camera_input, tick_timer.extrapolation(), frame_time)?;
+  }
+
   Ok(())
 }
