4badb95ffbbec753f9d453d30d0f7d5ece015b69
Gabriel Konat <gabrielkonat@gmail.com>
2022-03-06 12:05:25 +0100

    Support point debug rendering.

--- core/gfx/src/debug_renderer/mod.rs
+++ core/gfx/src/debug_renderer/mod.rs
@@ -14,13 +14,14 @@ pub struct DebugRenderer {
   uniform_buffer: GfxBuffer,
   uniform_bind_group: BindGroup,
 
-  line_list_render_pipeline: Option<DebugRendererPipeline>,
-  line_strip_render_pipeline: Option<DebugRendererPipeline>,
+  point_list_render_pipeline: Option<DebugRendererPipeline<PointVertex>>,
+  line_list_render_pipeline: Option<DebugRendererPipeline<RegularVertex>>,
+  line_strip_render_pipeline: Option<DebugRendererPipeline<RegularVertex>>,
 }
 
 impl DebugRenderer {
   pub fn request_features() -> Features {
-    Features::POLYGON_MODE_LINE
+    Features::POLYGON_MODE_POINT | Features::POLYGON_MODE_LINE
   }
 
   pub fn new(gfx: &Gfx, view_projection: Mat4) -> Self {
@@ -38,6 +39,17 @@ impl DebugRenderer {
       .with_layout_label("Debug uniform bind group layout")
       .with_label("Debug uniform bind group")
       .build(&gfx.device);
+
+    let has_polygon_mode_point_feature = gfx.adapter.features().contains(Features::POLYGON_MODE_POINT);
+    let point_list_render_pipeline = has_polygon_mode_point_feature.then(|| DebugRendererPipeline::new(
+      gfx,
+      &vertex_shader_module,
+      &fragment_shader_module,
+      &uniform_bind_group_layout,
+      PrimitiveTopology::PointList,
+      PolygonMode::Point,
+      "point list",
+    ));
 
     let has_polygon_mode_line_feature = gfx.adapter.features().contains(Features::POLYGON_MODE_LINE);
     let line_list_render_pipeline = has_polygon_mode_line_feature.then(|| DebugRendererPipeline::new(
@@ -62,56 +74,68 @@ impl DebugRenderer {
     Self {
       uniform_buffer,
       uniform_bind_group,
+      point_list_render_pipeline,
       line_list_render_pipeline,
       line_strip_render_pipeline,
     }
   }
 
+  pub fn draw_point(&mut self, point: Vec3, col: Vec4, size: f32) {
+    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+      pipeline.vertices.push(PointVertex::new(point, col, size));
+      pipeline.upload_new_vertex_buffer = true;
+    }
+  }
+
   pub fn draw_line(&mut self, start_pos: Vec3, end_pos: Vec3, start_col: Vec4, end_col: Vec4) {
     if let Some(pipeline) = &mut self.line_list_render_pipeline {
-      pipeline.vertices.push(Vertex::new(start_pos, start_col));
-      pipeline.vertices.push(Vertex::new(end_pos, end_col));
+      pipeline.vertices.push(RegularVertex::new(start_pos, start_col));
+      pipeline.vertices.push(RegularVertex::new(end_pos, end_col));
       pipeline.upload_new_vertex_buffer = true;
     }
   }
 
   pub fn draw_cube(&mut self, min: Vec3, size: f32, col: Vec4) {
     if let Some(pipeline) = &mut self.line_list_render_pipeline {
-      pipeline.vertices.push(Vertex::new(min, col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, 0.0, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min, col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, size, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min, col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, 0.0, size), col));
+      pipeline.vertices.push(RegularVertex::new(min, col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, 0.0, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min, col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, size, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min, col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, 0.0, size), col));
 
       let max = min + Vec3::new(size, size, size);
-      pipeline.vertices.push(Vertex::new(max, col));
-      pipeline.vertices.push(Vertex::new(max + Vec3::new(-size, 0.0, 0.0), col));
-      pipeline.vertices.push(Vertex::new(max, col));
-      pipeline.vertices.push(Vertex::new(max + Vec3::new(0.0, -size, 0.0), col));
-      pipeline.vertices.push(Vertex::new(max, col));
-      pipeline.vertices.push(Vertex::new(max + Vec3::new(0.0, 0.0, -size), col));
+      pipeline.vertices.push(RegularVertex::new(max, col));
+      pipeline.vertices.push(RegularVertex::new(max + Vec3::new(-size, 0.0, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(max, col));
+      pipeline.vertices.push(RegularVertex::new(max + Vec3::new(0.0, -size, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(max, col));
+      pipeline.vertices.push(RegularVertex::new(max + Vec3::new(0.0, 0.0, -size), col));
 
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, size, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, size, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, size, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, size, size), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, size, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, size, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, size, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, size, size), col));
 
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, 0.0, size), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, 0.0, size), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, 0.0, size), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(0.0, size, size), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, 0.0, size), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, 0.0, size), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, 0.0, size), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(0.0, size, size), col));
 
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, 0.0, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, size, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, 0.0, 0.0), col));
-      pipeline.vertices.push(Vertex::new(min + Vec3::new(size, 0.0, size), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, 0.0, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, size, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, 0.0, 0.0), col));
+      pipeline.vertices.push(RegularVertex::new(min + Vec3::new(size, 0.0, size), col));
 
       pipeline.upload_new_vertex_buffer = true;
     }
   }
 
   pub fn clear(&mut self) {
+    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+      pipeline.vertices.clear();
+      pipeline.upload_new_vertex_buffer = true;
+    }
     if let Some(pipeline) = &mut self.line_list_render_pipeline {
       pipeline.vertices.clear();
       pipeline.upload_new_vertex_buffer = true;
@@ -129,6 +153,9 @@ impl DebugRenderer {
 
     self.uniform_buffer.write_whole_data(&gfx.queue, &[Uniform { view_projection }]);
 
+    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+      pipeline.reupload_vertex_buffer_if_needed(gfx);
+    }
     if let Some(pipeline) = &mut self.line_list_render_pipeline {
       pipeline.reupload_vertex_buffer_if_needed(gfx);
     }
@@ -140,6 +167,9 @@ impl DebugRenderer {
       .with_label("Debug render pass")
       .begin_render_pass_for_swap_chain_with_load(frame.encoder, &frame.output_texture);
     render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
+    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+      pipeline.draw(&mut render_pass);
+    }
     if let Some(pipeline) = &mut self.line_list_render_pipeline {
       pipeline.draw(&mut render_pass);
     }
@@ -151,15 +181,15 @@ impl DebugRenderer {
 
 // Pipeline
 
-pub struct DebugRendererPipeline {
+struct DebugRendererPipeline<V> {
   render_pipeline: RenderPipeline,
   vertex_buffer: GfxBuffer,
   upload_new_vertex_buffer: bool,
-  vertices: Vec<Vertex>,
+  vertices: Vec<V>,
   label: &'static str,
 }
 
-impl DebugRendererPipeline {
+impl<V: Vertex + Pod> DebugRendererPipeline<V> {
   fn new(
     gfx: &Gfx,
     vertex_shader_module: &ShaderModule,
@@ -172,7 +202,7 @@ impl DebugRendererPipeline {
     let (_, render_pipeline) = RenderPipelineBuilder::new(&vertex_shader_module)
       .with_bind_group_layouts(&[&uniform_bind_group_layout])
       .with_default_premultiplied_alpha_blending_fragment_state(&fragment_shader_module, &gfx.surface)
-      .with_vertex_buffer_layouts(&[Vertex::buffer_layout()])
+      .with_vertex_buffer_layouts(&[V::buffer_layout()])
       .with_primitive_topology(primitive_topology)
       .with_polygon_mode(polygon_mode)
       .with_cull_mode(None)
@@ -222,26 +252,66 @@ struct Uniform {
 
 // Vertex data
 
+trait Vertex {
+  fn buffer_layout() -> VertexBufferLayout<'static>;
+}
+
+// Point vertex data
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
-struct Vertex {
+struct PointVertex {
+  pos: Vec3,
+  col: Vec4,
+  size: f32,
+}
+
+impl Vertex for PointVertex {
+  fn buffer_layout() -> VertexBufferLayout<'static> {
+    const ATTRIBUTES: &[VertexAttribute] = &wgpu::vertex_attr_array![
+      0 => Float32x3,
+      1 => Float32x4,
+      3 => Float32
+    ];
+    VertexBufferLayout {
+      array_stride: size_of::<Self>() as BufferAddress,
+      step_mode: VertexStepMode::Vertex,
+      attributes: ATTRIBUTES,
+    }
+  }
+}
+
+impl PointVertex {
+  #[inline]
+  fn new(pos: Vec3, col: Vec4, size: f32) -> Self {
+    Self { pos, col, size }
+  }
+}
+
+// Regular vertex data
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
+struct RegularVertex {
   pos: Vec3,
   col: Vec4,
 }
 
-impl Vertex {
+impl Vertex for RegularVertex {
   fn buffer_layout() -> VertexBufferLayout<'static> {
     const ATTRIBUTES: &[VertexAttribute] = &wgpu::vertex_attr_array![
       0 => Float32x3,
       1 => Float32x4,
     ];
     VertexBufferLayout {
-      array_stride: size_of::<Vertex>() as BufferAddress,
+      array_stride: size_of::<Self>() as BufferAddress,
       step_mode: VertexStepMode::Vertex,
       attributes: ATTRIBUTES,
     }
   }
+}
 
+impl RegularVertex {
   #[inline]
   fn new(pos: Vec3, col: Vec4) -> Self {
     Self { pos, col }
