83074496b4e47c02cd23bc258a3a66744840ec83
Gabriel Konat <gabrielkonat@gmail.com>
2022-06-03 13:45:37 +0200

    Alternative job queue implementation.

--- gadget/job_queue/src/main.rs
+++ gadget/job_queue/src/main.rs
@@ -1,72 +1,48 @@
-use std::iter;
-use std::time::Duration;
-
-use crossbeam_deque::{Injector, Stealer, Worker};
-use crossbeam_utils::{Backoff, thread};
+use crossbeam_channel::{Receiver, select, Sender, unbounded};
+use crossbeam_utils::thread;
 
 fn main() {
-  let global = Injector::new();
-
-  let mut workers = Vec::new();
-  let mut stealers = Vec::new();
-  for _ in 0..8 {
-    let worker = Worker::new_fifo();
-    let stealer = worker.stealer();
-    workers.push(worker);
-    stealers.push(stealer);
-  }
+  let (external_sender, external_receiver): (Sender<i32>, Receiver<i32>) = unbounded();
 
   thread::scope(|s| {
-    let mut threads = Vec::new();
-    for (i, local) in workers.into_iter().enumerate() {
-      let stealers: Vec<_> = stealers.iter().cloned().enumerate().filter_map(|(idx, s)| (i != idx).then_some(s)).collect();
-      threads.push(s.spawn(|_| {
-        let local = local;
-        let stealers = stealers;
-        let backoff = Backoff::new();
+    let (manager_to_worker_sender, manager_to_worker_receiver) = unbounded();
+    let (worker_to_manager_sender, worker_to_manager_receiver) = unbounded();
+    s.spawn(move |_| {
+      loop {
+        select! {
+          recv(external_receiver) -> task => {
+            if let Ok(task) = task {
+              println!("Submitting task {}", task);
+              manager_to_worker_sender.send(task).unwrap();
+            }
+          },
+          recv(worker_to_manager_receiver) -> value => {
+            if let Ok(value) = value {
+              println!("Completed task {}", value);
+            }
+          },
+        }
+      }
+    });
+
+    for _ in 0..8 {
+      let manager_to_worker_receiver = manager_to_worker_receiver.clone();
+      let worker_to_manager_sender = worker_to_manager_sender.clone();
+      s.spawn(move |_| {
         loop {
-          if let Some(task) = find_task(&local, &global, &stealers) {
-            println!("{}", task);
-            backoff.reset();
-          } else if backoff.is_completed() {
-            std::thread::park()
-          } else {
-            backoff.snooze();
+          if let Ok(task) = manager_to_worker_receiver.recv() {
+            let mut value = task;
+            for i in 0..4096 {
+              value = value.wrapping_add(value.wrapping_pow(i));
+            }
+            worker_to_manager_sender.send(value).unwrap();
           }
         }
-      }));
+      });
     }
 
     for i in 0..4096 {
-      global.push(i);
-    }
-    for thread in &threads {
-      thread.thread().unpark();
-    }
-
-    std::thread::sleep(Duration::from_secs(10));
-    
-    for i in 4096..8192 {
-      global.push(i);
-    }
-    for thread in &threads {
-      thread.thread().unpark();
+      external_sender.send(i).unwrap();
     }
   }).unwrap();
-}
-
-#[inline]
-fn find_task<T>(
-  local: &Worker<T>,
-  global: &Injector<T>,
-  stealers: &[Stealer<T>],
-) -> Option<T> {
-  local.pop().or_else(|| {
-    iter::repeat_with(|| {
-      global.steal_batch_and_pop(local)
-        .or_else(|| stealers.iter().map(|s| s.steal()).collect())
-    })
-      .find(|s| !s.is_retry())
-      .and_then(|s| s.success())
-  })
 }
