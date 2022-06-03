aaf2c78cdbff1299c81469bbda89d2b5ce080f8b
Gabriel Konat <gabrielkonat@gmail.com>
2022-06-03 12:21:45 +0200

    Add parking.

--- gadget/job_queue/src/main.rs
+++ gadget/job_queue/src/main.rs
@@ -1,7 +1,8 @@
 use std::iter;
+use std::time::Duration;
 
 use crossbeam_deque::{Injector, Stealer, Worker};
-use crossbeam_utils::thread;
+use crossbeam_utils::{Backoff, thread};
 
 fn main() {
   let global = Injector::new();
@@ -16,21 +17,40 @@ fn main() {
   }
 
   thread::scope(|s| {
+    let mut threads = Vec::new();
     for (i, local) in workers.into_iter().enumerate() {
       let stealers: Vec<_> = stealers.iter().cloned().enumerate().filter_map(|(idx, s)| (i != idx).then_some(s)).collect();
-      s.spawn(|_| {
+      threads.push(s.spawn(|_| {
         let local = local;
         let stealers = stealers;
+        let backoff = Backoff::new();
         loop {
           if let Some(task) = find_task(&local, &global, &stealers) {
             println!("{}", task);
+            backoff.reset();
+          } else if backoff.is_completed() {
+            std::thread::park()
+          } else {
+            backoff.snooze();
           }
         }
-      });
+      }));
     }
 
     for i in 0..4096 {
       global.push(i);
+    }
+    for thread in &threads {
+      thread.thread().unpark();
+    }
+
+    std::thread::sleep(Duration::from_secs(10));
+    
+    for i in 4096..8192 {
+      global.push(i);
+    }
+    for thread in &threads {
+      thread.thread().unpark();
     }
   }).unwrap();
 }
