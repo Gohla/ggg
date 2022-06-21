134652a6424708aa52104a97444bb693a77dd208
Gabriel Konat <gabrielkonat@gmail.com>
2022-06-21 13:36:47 +0200

    Optimize job scheduling.

--- core/job_queue/src/manager.rs
+++ core/job_queue/src/manager.rs
@@ -1,10 +1,13 @@
+use std::hash::BuildHasherDefault;
 use std::thread;
 use std::thread::JoinHandle;
 
 use flume::{Receiver, Sender};
+use linked_hash_map::LinkedHashMap;
+use linked_hash_set::LinkedHashSet;
 use petgraph::prelude::*;
 use petgraph::visit::VisitMap;
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::{FxHasher, FxHashMap, FxHashSet};
 use tracing::trace;
 
 use crate::{DepKey, In, Job, JobKey, JobQueueMessage, Out};
@@ -28,12 +31,12 @@ pub(super) struct ManagerThread<JK, DK, I, J, O> {
   from_worker: Receiver<FromWorker<JK, DK, O>>,
   to_queue: Sender<JobQueueMessage<JK, I, O>>,
 
-  worker_thread_job_buffer_count: usize,
+  target_running_job_count: usize,
 
   job_graph: DiGraphMap<JK, DK>,
-  job_key_to_job_status: FxHashMap<JK, JobStatus<I, O>>,
-  jobs_to_add: FxHashMap<JK, J>,
-  jobs_to_schedule: FxHashMap<JK, (DK, O)>,
+  job_key_to_job_status: FxHashMap<JK, JobStatus<DK, I, O>>,
+  jobs_to_add: LinkedHashMap<JK, J, BuildHasherDefault<FxHasher>>,
+  jobs_to_run: LinkedHashSet<JK, BuildHasherDefault<FxHasher>>,
 
   dependency_output_cache: Vec<Vec<(DK, O)>>,
   bfs_cache: Bfs<JK, FxHashSet<JK>>,
@@ -49,7 +52,7 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
     to_worker: Sender<crate::worker::FromManager<JK, DK, I, O>>,
     from_worker: Receiver<FromWorker<JK, DK, O>>,
     to_queue: Sender<JobQueueMessage<JK, I, O>>,
-    worker_thread_job_buffer_count: usize,
+    target_running_job_count: usize,
     dependency_output_cache_count: usize,
   ) -> Self {
     Self {
@@ -58,12 +61,12 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
       from_worker,
       to_queue,
 
-      worker_thread_job_buffer_count,
+      target_running_job_count,
 
       job_graph: DiGraphMap::new(),
       job_key_to_job_status: FxHashMap::default(),
-      jobs_to_add: FxHashMap::default(),
-      jobs_to_schedule: FxHashMap::default(),
+      jobs_to_add: LinkedHashMap::default(),
+      jobs_to_run: LinkedHashSet::default(),
 
       dependency_output_cache: Vec::with_capacity(dependency_output_cache_count),
       bfs_cache: Bfs::default(),
@@ -85,16 +88,15 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
     profiling::register_thread!();
     trace!("Started job queue manager thread");
     let mut job_key_cache_1 = Vec::new();
-    let mut job_key_cache_2 = Vec::new();
     loop {
-      let r#continue = self.receive(&mut job_key_cache_1, &mut job_key_cache_2);
+      let r#continue = self.receive(&mut job_key_cache_1);
       if !r#continue { break; }
     }
     trace!("Stopped job queue manager thread");
   }
 
   #[inline]
-  fn receive(&mut self, job_key_cache_1: &mut Vec<JK>, job_key_cache_2: &mut Vec<JK>) -> bool {
+  fn receive(&mut self, job_key_cache_1: &mut Vec<JK>) -> bool {
     let selected = flume::Selector::new()
       .recv(&self.from_worker, |result| {
         if let Ok(result) = result {
@@ -112,101 +114,92 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
       })
       .wait();
     match selected {
-      Some(SelectedReceiver::FromWorker((job_key, output, dependency_outputs))) => self.handle_from_worker(job_key, output, dependency_outputs, job_key_cache_1, job_key_cache_2),
-      Some(SelectedReceiver::FromQueue(message)) => self.handle_from_queue(message, job_key_cache_1, job_key_cache_2),
+      Some(SelectedReceiver::FromWorker((job_key, output, dependency_outputs))) => self.handle_from_worker(job_key, output, dependency_outputs, job_key_cache_1),
+      Some(SelectedReceiver::FromQueue(message)) => self.handle_from_queue(message, job_key_cache_1),
       None => false,
     }
   }
 
 
   #[inline]
-  fn handle_from_queue(&mut self, message: FromQueueMessage<JK, J>, job_key_cache_1: &mut Vec<JK>, job_key_cache_2: &mut Vec<JK>) -> bool {
+  fn handle_from_queue(&mut self, message: FromQueueMessage<JK, J>, job_key_cache_1: &mut Vec<JK>) -> bool {
     use FromQueueMessage::*;
     match message {
-      TryAddJob(job) => self.try_add_job(job, job_key_cache_1, job_key_cache_2),
+      TryAddJob(job) => self.try_add_job(job),
       TryRemoveJobAndOrphanedDependencies(job_key) => self.try_remove_job_and_orphaned_dependencies(job_key, job_key_cache_1),
     }
   }
 
   #[inline]
-  fn handle_from_worker(&mut self, job_key: JK, output: O, mut dependency_outputs: Vec<(DK, O)>, job_key_cache_1: &mut Vec<JK>, job_key_cache_2: &mut Vec<JK>) -> bool {
-    if self.dependency_output_cache.len() < self.dependency_output_cache.capacity() {
-      dependency_outputs.clear();
-      self.dependency_output_cache.push(dependency_outputs);
-    }
+  fn handle_from_worker(&mut self, job_key: JK, output: O, dependency_outputs: Vec<(DK, O)>, job_key_cache_1: &mut Vec<JK>) -> bool {
+    self.reclaim_dependency_outputs(dependency_outputs);
     use JobStatus::*;
     match self.job_key_to_job_status.get(&job_key) {
-      Some(Pending(_)) => return true, // Job was removed, added, and not scheduled while it was running -> don't complete it.
+      Some(Pending(_, _)) => return true, // Job was removed, added, and not scheduled while it was running -> don't complete it.
       Some(Completed(_)) => return true, // Job was removed, added, scheduled, and completed while it was running -> don't complete it.
       None => return true, // Job was removed while it was running -> don't complete it.
       _ => {} // Otherwise: continue.
     }
-    // Complete job.
-    if !self.complete_job(job_key, output) { return false; }
-    // Try to schedule dependent jobs.
+    // Try to make dependent jobs ready to run.
     job_key_cache_1.clear();
     self.job_graph.neighbors_directed(job_key, Incoming).collect_into(job_key_cache_1);
-    for dependent_node_index in job_key_cache_1.drain(..) {
-      if !self.try_schedule_job(dependent_node_index, job_key_cache_2) { return false; }
+    for depender_job_key in job_key_cache_1.drain(..) {
+      if !self.try_make_job_ready_to_run(depender_job_key, job_key, &output) { return false; }
     }
-    // Fill up `to_worker` to `worker_thread_job_buffer_count`.
-    while self.to_worker.len() < self.worker_thread_job_buffer_count && !self.jobs_to_add.is_empty() {
-      // OPTO: get rid of key copy and two unwraps; not possible with HashMap though?
-      let top_job_key = *self.jobs_to_add.keys().next().unwrap(); // Unwrap OK: jobs_to_add is not empty.
-      let job = self.jobs_to_add.remove(&top_job_key).unwrap(); // Unwrap OK: jobs_to_add contains that key.
-      if !self.try_add_job(job, job_key_cache_1, job_key_cache_2) { return false; }
-    }
-    true
+    // Complete job.
+    if !self.complete_job(job_key, output) { return false; }
+    // Run and add jobs up to the target.
+    self.run_and_add_jobs_until_target()
   }
 
 
   #[profiling::function]
   #[inline]
-  fn try_add_job(&mut self, job: J, job_key_cache_1: &mut Vec<JK>, job_key_cache_2: &mut Vec<JK>) -> bool {
+  fn try_add_job(&mut self, job: J) -> bool {
     let job_key = job.key();
     if self.job_key_to_job_status.contains_key(job_key) { return true; } // Job already exists in graph: done
     if self.jobs_to_add.contains_key(job_key) { return true; } // Job already exists in jobs to add map: done.
-    if self.to_worker.len() >= self.worker_thread_job_buffer_count {
+    if self.to_worker.len() >= self.target_running_job_count {
       // Number of jobs sent to workers is at level, don't add and schedule the job yet.
       self.jobs_to_add.insert(*job_key, job);
       return true;
     }
-    job_key_cache_1.clear();
-    self.force_add_job_and_dependencies(job, job_key_cache_1, None);
-    for job_key in job_key_cache_1.drain(..) {
-      // Try to schedule job.
-      if !self.try_schedule_job(job_key, job_key_cache_2) { return false; }
-    }
-    true
+    self.force_add_job_and_dependencies(job);
+    self.run_jobs_until_target()
   }
 
-  #[inline(always)] // Inline always for tail call recursion?
-  fn force_add_job_and_dependencies(&mut self, job: J, jobs_to_schedule: &mut Vec<JK>, depender_keys: Option<(DK, JK)>) {
+  #[inline]
+  fn force_add_job_and_dependencies(&mut self, job: J) -> Option<O> {
     let job_key = job.key();
-    if self.job_key_to_job_status.contains_key(job_key) {
-      // Job already exists in graph; only add dependency if needed.
-      if let Some((dependency_key, depender_job_key)) = depender_keys {
-        self.add_dependency_edge(depender_job_key, *job_key, dependency_key);
-      }
-      return;
+    if let Some(job_status) = self.job_key_to_job_status.get(job_key) { // Job already exists.
+      return job_status.clone_output_if_completed();
     }
     self.jobs_to_add.remove(job_key); // Remove from jobs_to_add, as we are force adding it.
     let job_key = *job_key;
     let (input, dependencies) = job.into();
     self.job_graph.add_node(job_key);
-    self.job_key_to_job_status.insert(job_key, JobStatus::Pending(input));
     trace!("Added job {:?}", job_key);
-    if let Some((dependency_key, depender_job_key)) = depender_keys {
-      self.add_dependency_edge(depender_job_key, job_key, dependency_key);
-    }
-    self.pending_jobs += 1;
-    jobs_to_schedule.push(job_key); // OPTO: try schedule jobs inline, as we have dependency information here?
+    let mut dependency_outputs = self.create_dependency_outputs();
+    let mut can_run = true;
     for (dependency_key, dependency_job) in dependencies {
-      self.force_add_job_and_dependencies(dependency_job, jobs_to_schedule, Some((dependency_key, job_key))); // Tail call recursion?
+      let dependency_job_key = *dependency_job.key();
+      let dependency_output = self.force_add_job_and_dependencies(dependency_job);
+      self.add_dependency_edge(job_key, dependency_job_key, dependency_key);
+      if let Some(dependency_output) = dependency_output {
+        dependency_outputs.push((dependency_key, dependency_output));
+      } else {
+        can_run = false;
+      }
     }
+    self.job_key_to_job_status.insert(job_key, JobStatus::Pending(input, dependency_outputs));
+    self.pending_jobs += 1;
+    if can_run {
+      self.jobs_to_run.insert(job_key);
+    }
+    None
   }
 
-  #[inline(always)]
+  #[inline]
   fn add_dependency_edge(&mut self, depender_job_key: JK, dependee_job_key: JK, dependency_key: DK) {
     self.job_graph.add_edge(depender_job_key, dependee_job_key, dependency_key);
     trace!("Added dependency {:?} from job {:?} to {:?}", dependency_key, depender_job_key, dependee_job_key);
@@ -223,6 +216,7 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
       return true;
     }
     trace!("Try to remove job {:?} along with orphaned dependencies", job_key);
+    // OPTO: implement custom BFS that ignores entire subgraphs of nodes that have incoming dependencies.
     // Reset BFS traversal (using leaky API as it provides no API for this)
     self.bfs_cache.discovered.clear();
     self.bfs_cache.stack.clear();
@@ -240,10 +234,12 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
         continue; // Job has incoming dependencies, can't remove it.
       }
       self.job_graph.remove_node(j);
+      self.jobs_to_run.remove(&j);
       trace!("Removed job {:?}", j);
       let job_status = self.job_key_to_job_status.remove(&j).unwrap(); // Unwrap OK: mapping must exist.
       let send_success = match job_status {
-        JobStatus::Pending(input) => {
+        JobStatus::Pending(input, dependency_outputs) => {
+          self.reclaim_dependency_outputs(dependency_outputs);
           let send_success = self.to_queue.send(JobQueueMessage::PendingJobRemoved(j, input)).is_ok();
           let send_success = self.decrement_pending_jobs_and_send_queue_empty_if_applicable() | send_success;
           send_success
@@ -266,35 +262,53 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
 
   #[profiling::function]
   #[inline]
-  fn try_schedule_job(&mut self, job_key: JK, job_key_cache: &mut Vec<JK>) -> bool {
-    trace!("Try to schedule job {:?}", job_key);
-    if let JobStatus::Pending(_) = self.job_key_to_job_status[&job_key] {
-      job_key_cache.clear();
-      self.job_graph.neighbors_directed(job_key, Outgoing).collect_into(job_key_cache);
-      if job_key_cache.iter().all(|j| self.job_key_to_job_status[j].is_completed()) {
-        let mut dependency_outputs = self.dependency_output_cache.pop().unwrap_or_else(|| Vec::new());
-        for dependency_job_key in job_key_cache.drain(..) {
-          let dependency_output = self.job_key_to_job_status[&dependency_job_key].clone_output_completed();
-          let dependency_edge = self.job_graph[(job_key, dependency_job_key)];
-          dependency_outputs.push((dependency_edge, dependency_output));
-        }
-        return self.schedule_job(job_key, dependency_outputs);
+  fn try_make_job_ready_to_run(&mut self, depender_job_key: JK, dependee_job_key: JK, dependee_job_output: &O) -> bool {
+    trace!("Try to make job {:?} ready to run due to completion of {:?}", depender_job_key, dependee_job_key);
+    if let JobStatus::Pending(_, dependency_outputs) = self.job_key_to_job_status.get_mut(&depender_job_key).unwrap() { // Unwrap OK: job exists.
+      let dependency_key = self.job_graph[(depender_job_key, dependee_job_key)];
+      dependency_outputs.push((dependency_key, dependee_job_output.clone()));
+      if self.job_graph.neighbors_directed(depender_job_key, Outgoing).count() == dependency_outputs.len() {
+        self.jobs_to_run.insert(depender_job_key);
       }
     }
     true
   }
 
   #[inline]
-  fn schedule_job(&mut self, job_key: JK, dependencies: Vec<(DK, O)>) -> bool {
-    let job_status = self.job_key_to_job_status.get_mut(&job_key).unwrap(); // Unwrap OK: job must exist when schedule_job is called.
-    if let JobStatus::Pending(input) = std::mem::replace(job_status, JobStatus::Running) {
-      trace!("Scheduling job {:?}", job_key);
+  fn run_pending_job(&mut self, job_key: JK) -> bool {
+    let job_status = self.job_key_to_job_status.get_mut(&job_key).unwrap(); // Unwrap OK: job must exist when `run_pending_job` is called.
+    if let JobStatus::Pending(input, dependency_outputs) = std::mem::replace(job_status, JobStatus::Running) {
+      trace!("Running job {:?}", job_key);
       self.pending_jobs -= 1;
       self.running_jobs += 1;
-      return self.to_worker.send((job_key, input, dependencies)).is_ok();
+      if !self.to_worker.send((job_key, input, dependency_outputs)).is_ok() { return false; }
     }
     true
   }
+
+
+  #[inline]
+  fn run_jobs_until_target(&mut self) -> bool {
+    while !self.jobs_to_run.is_empty() && self.to_worker.len() < self.target_running_job_count {
+      let job_key = self.jobs_to_run.pop_front().unwrap(); // Unwrap OK: `jobs_to_run` is not empty.
+      if !self.run_pending_job(job_key) { return false; }
+    }
+    true
+  }
+
+  #[inline]
+  fn run_and_add_jobs_until_target(&mut self) -> bool {
+    // First run jobs until target, to give jobs that are ready to run priority.
+    if !self.run_jobs_until_target() { return false; }
+    // Then add and run jobs until target.
+    while !self.jobs_to_add.is_empty() && self.to_worker.len() < self.target_running_job_count {
+      let (_, job) = self.jobs_to_add.pop_front().unwrap(); // Unwrap OK: `jobs_to_add` is not empty.
+      self.force_add_job_and_dependencies(job);
+      if !self.run_jobs_until_target() { return false; }
+    }
+    true
+  }
+
 
   #[inline]
   fn complete_job(&mut self, job_key: JK, output: O) -> bool {
@@ -330,30 +344,37 @@ impl<JK: JobKey, DK: DepKey, I: In, J: Job<JK, DK, I>, O: Out> ManagerThread<JK,
     }
     true
   }
+
+
+  #[inline]
+  fn reclaim_dependency_outputs(&mut self, mut dependency_outputs: Vec<(DK, O)>) {
+    if self.dependency_output_cache.len() < self.dependency_output_cache.capacity() {
+      dependency_outputs.clear();
+      self.dependency_output_cache.push(dependency_outputs);
+    }
+  }
+
+  #[inline]
+  fn create_dependency_outputs(&mut self) -> Vec<(DK, O)> {
+    self.dependency_output_cache.pop().unwrap_or_else(|| Vec::new())
+  }
 }
 
 
 // Job status
 
-pub(super) enum JobStatus<I, O> {
-  Pending(I),
+pub(super) enum JobStatus<DK, I, O> {
+  Pending(I, Vec<(DK, O)>),
   Running,
   Completed(O),
 }
 
-impl<I, O: Out> JobStatus<I, O> {
+impl<DK, I, O: Out> JobStatus<DK, I, O> {
   #[inline]
-  fn is_completed(&self) -> bool {
+  fn clone_output_if_completed(&self) -> Option<O> {
     match self {
-      Self::Completed(_) => true,
-      _ => false
-    }
-  }
-  #[inline]
-  fn clone_output_completed(&self) -> O {
-    match self {
-      Self::Completed(output) => output.clone(),
-      _ => panic!("Attempt to call `clone_output_completed` on non-`Completed` job status")
+      Self::Completed(output) => Some(output.clone()),
+      _ => None,
     }
   }
 }
