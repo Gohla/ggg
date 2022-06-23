55fa7e8dfa468ce10701442e54232a1e2630ee16
Gabriel Konat <gabrielkonat@gmail.com>
2022-06-23 16:22:45 +0200

    WIP AABB locational code.

--- core/voxel/src/lod/aabb.rs
+++ core/voxel/src/lod/aabb.rs
@@ -4,6 +4,207 @@ use ultraviolet::{UVec3, Vec3};
 
 use crate::chunk::size::ChunkSize;
 
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub struct Aabb32(u32);
+
+impl Aabb32 {
+  #[inline]
+  pub fn root() -> Self { Self(1) }
+  
+  #[inline]
+  pub fn depth(&self) -> u8 { ((32 - self.0.leading_zeros()) / 3) as u8 }
+  #[inline]
+  pub fn half_size(&self, max_half_size: u32) -> u32 { Self::half_size_internal(max_half_size, self.depth()) }
+  #[inline]
+  pub fn minimum_point(&self, max_half_size: u32) -> UVec3 {
+    let depth = self.depth();
+    let mut min = UVec3::zero();
+    let mut code = self.0;
+    let mut half_size = Self::half_size_internal(max_half_size, depth);
+    for _ in (0..depth).rev() {
+      let octant = code as u8 & 0b00000111;
+      min += Self::octant_to_min(octant, half_size);
+      code = code >> 3;
+      half_size = half_size << 1;
+    }
+    min
+  }
+
+  #[inline]
+  pub fn subdivide(&self) -> Aabb32Subdivide {
+    debug_assert!(self.0.leading_zeros() > 3, "Cannot subdivide {:?}, there is no space left in the locational code", self);
+    Aabb32Subdivide::new(self)
+  }
+  #[inline]
+  pub fn sibling_positive_x(&self) -> Option<Self> {
+    let depth = self.depth();
+    if depth == 0 { return None; }
+    let first_x_bit = 1 << 0;
+    if self.0 & first_x_bit != 0 { // Is X bit set on the first octant?
+      let code = self.0 & !first_x_bit; // Unset X bit on the first octant.
+      return Some(Self(code));
+    }
+    if depth == 1 { return None; } // Cannot go to parent.
+    let second_x_bit = 1 << 3;
+    if self.0 & second_x_bit != 0 { // Is X bit set on the second octant?
+      let code = self.0 & !second_x_bit; // Unset X bit on the second octant.
+      return Some(Self(code));
+    }
+    // There is a parent, but we cannot go to its positive x sibling as it is at the border.
+    None
+  }
+  #[inline]
+  pub fn sibling_positive_y(&self) -> Option<Self> {
+    let depth = self.depth();
+    if depth == 0 { return None; }
+    let first_y_bit = 1 << 1;
+    if self.0 & first_y_bit != 0 { // Is Y bit set on the first octant?
+      let code = self.0 & !first_y_bit; // Unset Y bit on the first octant.
+      return Some(Self(code));
+    }
+    if depth == 1 { return None; } // Cannot go to parent.
+    let second_y_bit = 1 << 4;
+    if self.0 & second_y_bit != 0 { // Is Y bit set on the second octant?
+      let code = self.0 & !second_y_bit; // Unset Y bit on the second octant.
+      return Some(Self(code));
+    }
+    // There is a parent, but we cannot go to its positive Y sibling as it is at the border.
+    None
+  }
+  #[inline]
+  pub fn sibling_positive_z(&self) -> Option<Self> {
+    let depth = self.depth();
+    if depth == 0 { return None; }
+    let first_z_bit = 1 << 2;
+    if self.0 & first_z_bit != 0 { // Is Z bit set on the first octant?
+      let code = self.0 & !first_z_bit; // Unset Z bit on the first octant.
+      return Some(Self(code));
+    }
+    if depth == 1 { return None; } // Cannot go to parent.
+    let second_z_bit = 1 << 5;
+    if self.0 & second_z_bit != 0 { // Is Z bit set on the second octant?
+      let code = self.0 & !second_z_bit; // Unset Z bit on the second octant.
+      return Some(Self(code));
+    }
+    // There is a parent, but we cannot go to its positive Z sibling as it is at the border.
+    None
+  }
+
+
+  #[inline]
+  fn half_size_internal(max_half_size: u32, depth: u8) -> u32 {
+    debug_assert!(max_half_size.is_power_of_two(), "Max half size {} is not a power of 2", max_half_size);
+    max_half_size >> depth // Right shift is divide by 2 for powers of 2.
+  }
+  #[inline]
+  fn octant_to_min(octant: u8, half_size: u32) -> UVec3 {
+    match octant {
+      0 => UVec3::new(half_size, half_size, half_size),
+      1 => UVec3::new(0, half_size, half_size),
+      2 => UVec3::new(half_size, 0, half_size),
+      3 => UVec3::new(0, 0, half_size),
+      4 => UVec3::new(half_size, half_size, 0),
+      5 => UVec3::new(0, half_size, 0),
+      6 => UVec3::new(half_size, 0, 0),
+      7 => UVec3::new(0, 0, 0),
+      _ => unreachable!(),
+    }
+  }
+}
+
+pub struct Aabb32Subdivide {
+  code: u32,
+  octant: u8,
+}
+
+impl Aabb32Subdivide {
+  #[inline]
+  fn new(aabb: &Aabb32) -> Self { Self { code: aabb.0 << 3, octant: 0 } }
+}
+
+impl Iterator for Aabb32Subdivide {
+  type Item = Aabb32;
+  #[inline]
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.octant > 7 { return None; }
+    let aabb = Aabb32(self.code | self.octant as u32);
+    self.octant += 1;
+    Some(aabb)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use ultraviolet::UVec3;
+
+  use crate::lod::aabb::Aabb32;
+
+  #[test]
+  fn root() {
+    let max_half_size = 2048;
+    let root = Aabb32::root();
+    assert_eq!(root.depth(), 0);
+    assert_eq!(root.half_size(max_half_size), 2048);
+    assert_eq!(root.minimum_point(max_half_size), UVec3::zero());
+  }
+
+  #[test]
+  fn subdivide_once() {
+    let max_half_size = 2048;
+    let root = Aabb32::root();
+    let subdivided: Vec<_> = root.subdivide().collect();
+    {
+      let sub = subdivided[0];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(1024, 1024, 1024));
+    }
+    {
+      let sub = subdivided[1];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(0, 1024, 1024));
+    }
+    {
+      let sub = subdivided[2];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(1024, 0, 1024));
+    }
+    {
+      let sub = subdivided[3];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(0, 0, 1024));
+    }
+    {
+      let sub = subdivided[4];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(1024, 1024, 0));
+    }
+    {
+      let sub = subdivided[5];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(0, 1024, 0));
+    }
+    {
+      let sub = subdivided[6];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(1024, 0, 0));
+    }
+    {
+      let sub = subdivided[7];
+      assert_eq!(sub.depth(), 1);
+      assert_eq!(sub.half_size(max_half_size), 1024);
+      assert_eq!(sub.minimum_point(max_half_size), UVec3::new(0, 0, 0));
+    }
+  }
+}
+
+
 /// Square axis-aligned bounding box, always in powers of 2, and with size always larger than 1.
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct AABB {
