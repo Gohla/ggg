6e514b138db077ea3e305690daf433d7999f27cf
Gabriel Konat <gabrielkonat@gmail.com>
2024-06-12 14:10:51 +0200

    Use growable buffers in debug renderer.

--- core/gfx/src/debug_renderer/mod.rs
+++ core/gfx/src/debug_renderer/mod.rs
@@ -2,22 +2,26 @@ use std::mem::size_of;
 
 use bytemuck::{Pod, Zeroable};
 use ultraviolet::{Mat4, Vec3, Vec4};
-use wgpu::{BindGroup, BindGroupLayout, BufferAddress, Features, IndexFormat, PolygonMode, PrimitiveTopology, RenderPass, RenderPipeline, ShaderModule, ShaderStages, VertexAttribute, VertexBufferLayout, VertexStepMode};
+use wgpu::{BindGroup, BufferAddress, Features, PolygonMode, PrimitiveTopology, ShaderStages, VertexAttribute, VertexBufferLayout, VertexStepMode};
+use wgpu::util::StagingBelt;
 
 use crate::{Frame, Gfx, include_shader};
 use crate::bind_group::CombinedBindGroupLayoutBuilder;
 use crate::buffer::{BufferBuilder, GfxBuffer};
+use crate::debug_renderer::pipeline::{Pipeline, Vertex};
 use crate::render_pass::RenderPassBuilder;
-use crate::render_pipeline::RenderPipelineBuilder;
+
+mod pipeline;
 
 pub struct DebugRenderer {
+  staging_belt: StagingBelt,
   uniform_buffer: GfxBuffer,
   uniform_bind_group: BindGroup,
 
-  point_list_render_pipeline: Option<DebugRendererPipeline<PointVertex>>,
-  line_list_render_pipeline: Option<DebugRendererPipeline<RegularVertex>>,
-  line_strip_render_pipeline: Option<DebugRendererPipeline<RegularVertex>>,
-  line_triangle_list_render_pipeline: Option<DebugRendererPipeline<RegularVertex>>,
+  point_list_pipeline: Option<Pipeline<PointVertex>>,
+  line_list_pipeline: Option<Pipeline<RegularVertex>>,
+  line_strip_pipeline: Option<Pipeline<RegularVertex>>,
+  line_triangle_list_pipeline: Option<Pipeline<RegularVertex>>,
 }
 
 impl DebugRenderer {
@@ -26,9 +30,7 @@ impl DebugRenderer {
   }
 
   pub fn new(gfx: &Gfx, view_projection: Mat4) -> Self {
-    let point_vertex_shader_module = gfx.device.create_shader_module(include_shader!("debug_renderer/point_vert"));
-    let vertex_shader_module = gfx.device.create_shader_module(include_shader!("debug_renderer/vert"));
-    let fragment_shader_module = gfx.device.create_shader_module(include_shader!("debug_renderer/frag"));
+    let staging_belt = StagingBelt::new(1024 * 1024); // 1 MiB chunk size
 
     let uniform_buffer = BufferBuilder::new()
       .with_uniform_usage()
@@ -42,8 +44,12 @@ impl DebugRenderer {
       .with_label("Debug uniform bind group")
       .build(&gfx.device);
 
+    let point_vertex_shader_module = gfx.device.create_shader_module(include_shader!("debug_renderer/point_vert"));
+    let vertex_shader_module = gfx.device.create_shader_module(include_shader!("debug_renderer/vert"));
+    let fragment_shader_module = gfx.device.create_shader_module(include_shader!("debug_renderer/frag"));
+
     let has_polygon_mode_point_feature = gfx.adapter.features().contains(Features::POLYGON_MODE_POINT);
-    let point_list_render_pipeline = has_polygon_mode_point_feature.then(|| DebugRendererPipeline::new(
+    let point_list_pipeline = has_polygon_mode_point_feature.then(|| Pipeline::new(
       gfx,
       &point_vertex_shader_module,
       &fragment_shader_module,
@@ -55,7 +61,7 @@ impl DebugRenderer {
     ));
 
     let has_polygon_mode_line_feature = gfx.adapter.features().contains(Features::POLYGON_MODE_LINE);
-    let line_list_render_pipeline = has_polygon_mode_line_feature.then(|| DebugRendererPipeline::new(
+    let line_list_pipeline = has_polygon_mode_line_feature.then(|| Pipeline::new(
       gfx,
       &vertex_shader_module,
       &fragment_shader_module,
@@ -65,7 +71,7 @@ impl DebugRenderer {
       PolygonMode::Line,
       "line list",
     ));
-    let line_strip_render_pipeline = has_polygon_mode_line_feature.then(|| DebugRendererPipeline::new(
+    let line_strip_pipeline = has_polygon_mode_line_feature.then(|| Pipeline::new(
       gfx,
       &vertex_shader_module,
       &fragment_shader_module,
@@ -75,7 +81,7 @@ impl DebugRenderer {
       PolygonMode::Line,
       "line strip",
     ));
-    let line_triangle_list_render_pipeline = has_polygon_mode_line_feature.then(|| DebugRendererPipeline::new(
+    let line_triangle_list_pipeline = has_polygon_mode_line_feature.then(|| Pipeline::new(
       gfx,
       &vertex_shader_module,
       &fragment_shader_module,
@@ -87,71 +93,64 @@ impl DebugRenderer {
     ));
 
     Self {
+      staging_belt,
       uniform_buffer,
       uniform_bind_group,
-      point_list_render_pipeline,
-      line_list_render_pipeline,
-      line_strip_render_pipeline,
-      line_triangle_list_render_pipeline,
+      point_list_pipeline,
+      line_list_pipeline,
+      line_strip_pipeline,
+      line_triangle_list_pipeline,
     }
   }
 
 
   pub fn draw_point(&mut self, point: Vec3, col: Vec4, size: f32) {
-    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+    if let Some(pipeline) = &mut self.point_list_pipeline {
       pipeline.push_vertex(PointVertex::new(point, col, size));
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_points(&mut self, positions: impl IntoIterator<Item=Vec3>, col: Vec4, size: f32) {
-    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+    if let Some(pipeline) = &mut self.point_list_pipeline {
       pipeline.push_vertices(positions.into_iter().map(|pos| PointVertex::new(pos, col, size)));
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_point_vertices(&mut self, vertices: impl IntoIterator<Item=PointVertex>) {
-    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+    if let Some(pipeline) = &mut self.point_list_pipeline {
       pipeline.push_vertices(vertices);
-      pipeline.upload_buffers = true;
     }
   }
 
 
   pub fn draw_line(&mut self, start_pos: Vec3, end_pos: Vec3, start_col: Vec4, end_col: Vec4) {
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.push_vertex(RegularVertex::new(start_pos, start_col));
       pipeline.push_vertex(RegularVertex::new(end_pos, end_col));
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_lines(&mut self, positions: impl IntoIterator<Item=Vec3>, col: Vec4) {
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.push_vertices(positions.into_iter().map(|pos| RegularVertex::new(pos, col)));
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_lines_indexed(&mut self, positions: impl IntoIterator<Item=Vec3>, indices: impl IntoIterator<Item=u32>, col: Vec4) {
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.push_vertices_and_indices(positions.into_iter().map(|pos| RegularVertex::new(pos, col)), indices);
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_line_vertices(&mut self, vertices: impl IntoIterator<Item=RegularVertex>) {
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.push_vertices(vertices);
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_line_vertices_indexed(&mut self, vertices: impl IntoIterator<Item=RegularVertex>, indices: impl IntoIterator<Item=u32>) {
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.push_vertices_and_indices(vertices, indices);
-      pipeline.upload_buffers = true;
     }
   }
 
@@ -207,214 +206,86 @@ impl DebugRenderer {
 
 
   pub fn draw_triangles_wireframe(&mut self, positions: impl IntoIterator<Item=Vec3>, col: Vec4) {
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
       pipeline.push_vertices(positions.into_iter().map(|pos| RegularVertex::new(pos, col)));
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_triangles_wireframe_indexed(&mut self, positions: impl IntoIterator<Item=Vec3>, indices: impl IntoIterator<Item=u32>, col: Vec4) {
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
       pipeline.push_vertices_and_indices(positions.into_iter().map(|pos| RegularVertex::new(pos, col)), indices);
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_triangle_vertices_wireframe(&mut self, vertices: impl IntoIterator<Item=RegularVertex>) {
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
       pipeline.push_vertices(vertices);
-      pipeline.upload_buffers = true;
     }
   }
 
   pub fn draw_triangle_vertices_wireframe_indexed(&mut self, vertices: impl IntoIterator<Item=RegularVertex>, indices: impl IntoIterator<Item=u32>) {
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
       pipeline.push_vertices_and_indices(vertices, indices);
-      pipeline.upload_buffers = true;
     }
   }
 
 
   pub fn clear(&mut self) {
-    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+    if let Some(pipeline) = &mut self.point_list_pipeline {
       pipeline.clear();
     }
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.clear();
     }
-    if let Some(pipeline) = &mut self.line_strip_render_pipeline {
+    if let Some(pipeline) = &mut self.line_strip_pipeline {
       pipeline.clear();
     }
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
       pipeline.clear();
     }
   }
 
   pub fn render<'a>(&mut self, gfx: &Gfx, frame: &mut Frame<'a>, model_view_projection: Mat4) {
-    if self.line_list_render_pipeline.is_none() && self.line_strip_render_pipeline.is_none() {
+    if self.point_list_pipeline.is_some() && self.line_list_pipeline.is_none() &&
+      self.line_strip_pipeline.is_none() && self.line_triangle_list_pipeline.is_none() {
       return; // Nothing to do
     }
 
+    self.staging_belt.recall();
+
     self.uniform_buffer.enqueue_write_all_data(&gfx.queue, &[Uniform { model_view_projection }]);
 
-    if let Some(pipeline) = &mut self.point_list_render_pipeline {
-      pipeline.upload_buffers_if_needed(gfx);
+    if let Some(pipeline) = &mut self.point_list_pipeline {
+      pipeline.write_buffers_if_needed(gfx, &mut frame.encoder, &mut self.staging_belt);
     }
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
-      pipeline.upload_buffers_if_needed(gfx);
+    if let Some(pipeline) = &mut self.line_list_pipeline {
+      pipeline.write_buffers_if_needed(gfx, &mut frame.encoder, &mut self.staging_belt);
     }
-    if let Some(pipeline) = &mut self.line_strip_render_pipeline {
-      pipeline.upload_buffers_if_needed(gfx);
+    if let Some(pipeline) = &mut self.line_strip_pipeline {
+      pipeline.write_buffers_if_needed(gfx, &mut frame.encoder, &mut self.staging_belt);
     }
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
-      pipeline.upload_buffers_if_needed(gfx);
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
+      pipeline.write_buffers_if_needed(gfx, &mut frame.encoder, &mut self.staging_belt);
     }
 
     let mut render_pass = RenderPassBuilder::new()
       .with_label("Debug render pass")
       .begin_render_pass_for_gfx_frame_with_load(gfx, frame, false);
     render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
-    if let Some(pipeline) = &mut self.point_list_render_pipeline {
+    if let Some(pipeline) = &mut self.point_list_pipeline {
       pipeline.draw(&mut render_pass);
     }
-    if let Some(pipeline) = &mut self.line_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_list_pipeline {
       pipeline.draw(&mut render_pass);
     }
-    if let Some(pipeline) = &mut self.line_strip_render_pipeline {
+    if let Some(pipeline) = &mut self.line_strip_pipeline {
       pipeline.draw(&mut render_pass);
     }
-    if let Some(pipeline) = &mut self.line_triangle_list_render_pipeline {
+    if let Some(pipeline) = &mut self.line_triangle_list_pipeline {
       pipeline.draw(&mut render_pass);
     }
-  }
-}
 
-// Pipeline
-
-struct DebugRendererPipeline<V> {
-  render_pipeline: RenderPipeline,
-  vertex_buffer: GfxBuffer,
-  index_buffer: GfxBuffer,
-  upload_buffers: bool,
-  vertices: Vec<V>,
-  indices: Vec<u32>,
-  label: &'static str,
-}
-
-impl<V: Vertex + Pod> DebugRendererPipeline<V> {
-  fn new(
-    gfx: &Gfx,
-    vertex_shader_module: &ShaderModule,
-    fragment_shader_module: &ShaderModule,
-    uniform_bind_group_layout: &BindGroupLayout,
-    multisample_sample_count: u32,
-    primitive_topology: PrimitiveTopology,
-    polygon_mode: PolygonMode,
-    label: &'static str,
-  ) -> Self {
-    let (_, render_pipeline) = RenderPipelineBuilder::new(&vertex_shader_module)
-      .with_bind_group_layouts(&[&uniform_bind_group_layout])
-      .with_default_premultiplied_alpha_blending_fragment_state(&fragment_shader_module, &gfx.surface)
-      .with_vertex_buffer_layouts(&[V::buffer_layout()])
-      .with_multisample_count(multisample_sample_count)
-      .with_primitive_topology(primitive_topology)
-      .with_polygon_mode(polygon_mode)
-      .with_cull_mode(None)
-      .with_layout_label(&format!("Debug {} pipeline layout", label))
-      .with_label(&format!("Debug {} render pipeline", label))
-      .build(&gfx.device);
-    let vertices = Vec::new();
-    let vertex_buffer = BufferBuilder::new()
-      .with_static_vertex_usage()
-      .with_label(&format!("Debug {} vertex buffer", label))
-      .create_with_data(&gfx.device, &vertices);
-    let indices = Vec::new();
-    let index_buffer = BufferBuilder::new()
-      .with_static_index_usage()
-      .with_label(&format!("Debug {} index buffer", label))
-      .create_with_data(&gfx.device, &indices);
-    Self {
-      render_pipeline,
-      vertex_buffer,
-      index_buffer,
-      upload_buffers: false,
-      vertices,
-      indices,
-      label,
-    }
-  }
-
-
-  #[inline]
-  fn push_vertex_without_index(&mut self, vertex: V) {
-    self.vertices.push(vertex);
-  }
-
-  #[inline]
-  fn next_index(&self) -> u32 {
-    self.vertices.len() as u32
-  }
-
-  #[inline]
-  fn push_vertex(&mut self, vertex: V) -> u32 {
-    let index = self.next_index();
-    self.push_index(index);
-    self.push_vertex_without_index(vertex);
-    index
-  }
-
-  #[inline]
-  fn push_index(&mut self, index: u32) {
-    self.indices.push(index);
-  }
-
-  #[inline]
-  fn push_vertices(&mut self, vertices: impl IntoIterator<Item=V>) {
-    let base = self.next_index();
-    self.vertices.extend(vertices);
-    self.indices.extend(base..self.next_index());
-  }
-
-  #[inline]
-  fn push_vertices_and_indices(&mut self, vertices: impl IntoIterator<Item=V>, indices: impl IntoIterator<Item=u32>) {
-    let base = self.vertices.len() as u32;
-    self.vertices.extend(vertices);
-    self.indices.extend(indices.into_iter().map(|idx| base + idx));
-  }
-
-
-  fn upload_buffers_if_needed(&mut self, gfx: &Gfx) {
-    if self.upload_buffers {
-      self.vertex_buffer = BufferBuilder::new()
-        .with_static_vertex_usage()
-        .with_label(&format!("Debug {} vertex buffer", self.label))
-        .create_with_data(&gfx.device, &self.vertices);
-      self.index_buffer = BufferBuilder::new()
-        .with_static_index_usage()
-        .with_label(&format!("Debug {} index buffer", self.label))
-        .create_with_data(&gfx.device, &self.indices);
-    }
-    self.upload_buffers = false;
-  }
-
-  fn draw<'a, 'b>(&'a self, render_pass: &'b mut RenderPass<'a>) {
-    render_pass.push_debug_group(&format!("Debug draw {}", self.label));
-    render_pass.set_pipeline(&self.render_pipeline);
-    render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-    if !self.indices.is_empty() {
-      render_pass.set_index_buffer(self.index_buffer.slice(..), IndexFormat::Uint32);
-      render_pass.draw_indexed(0..self.indices.len() as u32, 0, 0..1);
-    } else {
-      render_pass.draw(0..self.vertices.len() as u32, 0..1);
-    }
-    render_pass.pop_debug_group();
-  }
-
-
-  fn clear(&mut self) {
-    self.vertices.clear();
-    self.indices.clear();
-    self.upload_buffers = true;
+    self.staging_belt.finish();
   }
 }
 
@@ -424,12 +295,6 @@ impl<V: Vertex + Pod> DebugRendererPipeline<V> {
 #[derive(Debug, Copy, Clone, Pod, Zeroable)]
 struct Uniform {
   model_view_projection: Mat4,
-}
-
-// Vertex data
-
-trait Vertex {
-  fn buffer_layout() -> VertexBufferLayout<'static>;
 }
 
 // Point vertex data
@@ -444,9 +309,7 @@ pub struct PointVertex {
 
 impl PointVertex {
   #[inline]
-  pub fn new(pos: Vec3, col: Vec4, size: f32) -> Self {
-    Self { pos, col, size }
-  }
+  pub fn new(pos: Vec3, col: Vec4, size: f32) -> Self { Self { pos, col, size } }
 }
 
 impl Vertex for PointVertex {
@@ -475,9 +338,7 @@ pub struct RegularVertex {
 
 impl RegularVertex {
   #[inline]
-  pub fn new(pos: Vec3, col: Vec4) -> Self {
-    Self { pos, col }
-  }
+  pub fn new(pos: Vec3, col: Vec4) -> Self { Self { pos, col } }
 }
 
 impl Vertex for RegularVertex {
---
+++ core/gfx/src/debug_renderer/pipeline.rs
@@ -0,0 +1,132 @@
+use bytemuck::Pod;
+use wgpu::{BindGroupLayout, CommandEncoder, IndexFormat, PolygonMode, PrimitiveTopology, RenderPass, RenderPipeline, ShaderModule, VertexBufferLayout};
+use wgpu::util::StagingBelt;
+
+use crate::Gfx;
+use crate::growable_buffer::{GrowableBuffer, GrowableBufferBuilder};
+use crate::render_pipeline::RenderPipelineBuilder;
+
+pub trait Vertex {
+  fn buffer_layout() -> VertexBufferLayout<'static>;
+}
+
+pub struct Pipeline<V> {
+  render_pipeline: RenderPipeline,
+  vertex_buffer: GrowableBuffer<String>,
+  index_buffer: GrowableBuffer<String>,
+  write_buffers: bool,
+  vertices: Vec<V>,
+  indices: Vec<u32>,
+  label: &'static str,
+}
+
+impl<V: Vertex + Pod> Pipeline<V> {
+  pub fn new(
+    gfx: &Gfx,
+    vertex_shader_module: &ShaderModule,
+    fragment_shader_module: &ShaderModule,
+    uniform_bind_group_layout: &BindGroupLayout,
+    multisample_sample_count: u32,
+    primitive_topology: PrimitiveTopology,
+    polygon_mode: PolygonMode,
+    label: &'static str,
+  ) -> Self {
+    let (_, render_pipeline) = RenderPipelineBuilder::new(&vertex_shader_module)
+      .with_bind_group_layouts(&[&uniform_bind_group_layout])
+      .with_default_premultiplied_alpha_blending_fragment_state(&fragment_shader_module, &gfx.surface)
+      .with_vertex_buffer_layouts(&[V::buffer_layout()])
+      .with_multisample_count(multisample_sample_count)
+      .with_primitive_topology(primitive_topology)
+      .with_polygon_mode(polygon_mode)
+      .with_cull_mode(None)
+      .with_layout_label(&format!("Debug {} pipeline layout", label))
+      .with_label(&format!("Debug {} render pipeline", label))
+      .build(&gfx.device);
+    let vertex_buffer = GrowableBufferBuilder::new()
+      .with_vertex_usage()
+      .with_label(format!("Debug {} vertex buffer", label))
+      .create();
+    let index_buffer = GrowableBufferBuilder::new()
+      .with_index_usage()
+      .with_label(format!("Debug {} index buffer", label))
+      .create();
+    Self {
+      render_pipeline,
+      vertex_buffer,
+      index_buffer,
+      write_buffers: false,
+      vertices: Vec::default(),
+      indices: Vec::default(),
+      label,
+    }
+  }
+
+  #[inline]
+  pub fn push_vertex(&mut self, vertex: V) -> u32 {
+    let index = self.next_index();
+    self.push_index(index);
+    self.push_vertex_without_index(vertex);
+    self.write_buffers = true;
+    index
+  }
+
+  #[inline]
+  pub fn push_vertices(&mut self, vertices: impl IntoIterator<Item=V>) {
+    let base = self.next_index();
+    self.vertices.extend(vertices);
+    self.indices.extend(base..self.next_index());
+    self.write_buffers = true;
+  }
+
+  #[inline]
+  pub fn push_vertices_and_indices(&mut self, vertices: impl IntoIterator<Item=V>, indices: impl IntoIterator<Item=u32>) {
+    let base = self.vertices.len() as u32;
+    self.vertices.extend(vertices);
+    self.indices.extend(indices.into_iter().map(|idx| base + idx));
+    self.write_buffers = true;
+  }
+
+  pub fn clear(&mut self) {
+    self.vertices.clear();
+    self.indices.clear();
+  }
+
+  pub fn write_buffers_if_needed(&mut self, gfx: &Gfx, encoder: &mut CommandEncoder, staging_belt: &mut StagingBelt) {
+    if self.write_buffers {
+      self.vertex_buffer.write_data(&gfx.device, encoder, staging_belt, &self.vertices);
+      self.index_buffer.write_data(&gfx.device, encoder, staging_belt, &self.indices);
+    }
+    self.write_buffers = false;
+  }
+
+  pub fn draw<'a, 'b>(&'a self, render_pass: &'b mut RenderPass<'a>) {
+    if let (Some(vertex_buffer), Some(index_buffer)) = (self.vertex_buffer.backing_buffer(), self.index_buffer.backing_buffer()) {
+      render_pass.push_debug_group(&format!("Debug draw {}", self.label));
+      render_pass.set_pipeline(&self.render_pipeline);
+      render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+      if !self.indices.is_empty() {
+        render_pass.set_index_buffer(index_buffer.slice(..), IndexFormat::Uint32);
+        render_pass.draw_indexed(0..self.indices.len() as u32, 0, 0..1);
+      } else {
+        render_pass.draw(0..self.vertices.len() as u32, 0..1);
+      }
+      render_pass.pop_debug_group();
+    }
+  }
+
+
+  #[inline]
+  fn push_vertex_without_index(&mut self, vertex: V) {
+    self.vertices.push(vertex);
+  }
+
+  #[inline]
+  fn next_index(&self) -> u32 {
+    self.vertices.len() as u32
+  }
+
+  #[inline]
+  fn push_index(&mut self, index: u32) {
+    self.indices.push(index);
+  }
+}
